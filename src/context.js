29722be7752c6af200558f21077d1131e6732a69
Gun GUZEL <gun.guzeloglu@gmail.com>
2018-09-28 22:04:23 +0300

    End of project

--- src/context.js
+++ src/context.js
@@ -3,10 +3,24 @@ import axios from "axios";
 
 const Context = React.createContext();
 
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "SEARCH_TRACKS":
+      return {
+        ...state,
+        track_list: action.payload,
+        heading: "Search Results"
+      };
+    default:
+      return state;
+  }
+};
+
 export class Provider extends Component {
   state = {
     track_list: [],
-    heading: "Top 10 Tracks"
+    heading: "Top 10 Tracks",
+    dispatch: action => this.setState(state => reducer(state, action))
   };
 
   componentDidMount() {
@@ -15,7 +29,7 @@ export class Provider extends Component {
         `https://cors-anywhere.herokuapp.com/http://api.musixmatch.com/ws/1.1/chart.tracks.get?page=1&page_size=10&country=us&f_has_lyrics=1&apikey=7f42fdbb6ab1609796df015b0cf48dd5`
       )
       .then(res => {
-        console.log(res.data);
+        //console.log(res.data);
         this.setState({ track_list: res.data.message.body.track_list });
       })
       .catch(err => console.log(err));
